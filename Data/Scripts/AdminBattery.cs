db3e494c80479a9e16a6f86bab15e43f3c63973f
Sadragos <sadragos@13byte.de>
2021-03-13 16:35:32 +0100

    Migrated Repository

---
+++ Data/Scripts/AdminBattery.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sandbox.Common;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game;
+using Sandbox.Game.GameSystems.Conveyors;
+using Sandbox.Game.Entities;
+using Sandbox.Game.EntityComponents;
+using Sandbox.Definitions;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+
+using VRage;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+using VRageMath;
+using VRage.Game.Entity;
+using VRage.Voxels;
+using VRage.ModAPI;
+
+namespace DefenseTurrets
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_BatteryBlock), true, "AdminBattery")]
+    public class AdminBattery : MyGameLogicComponent
+    {
+
+        MyBatteryBlock TerminalalBlock;
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_100TH_FRAME;
+
+            TerminalalBlock = Entity as MyBatteryBlock;
+        }
+
+        public override void UpdateBeforeSimulation100()
+        {
+            base.UpdateBeforeSimulation100();
+            TerminalalBlock.CurrentStoredPower = TerminalalBlock.MaxStoredPower;
+        }
+    }
+}
---
+++ Data/Scripts/AdminMissile.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sandbox.Common;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game;
+using Sandbox.Game.GameSystems.Conveyors;
+using Sandbox.Game.Entities;
+using Sandbox.Game.EntityComponents;
+using Sandbox.Definitions;
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+
+using VRage;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+using VRageMath;
+using VRage.Game.Entity;
+using VRage.Voxels;
+
+namespace UnstableBlocks
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_LargeMissileTurret), true, "AdminMissileTurret")]
+    public class AdminMissileTurret : MyGameLogicComponent
+    {
+
+        Sandbox.ModAPI.IMyTerminalBlock TerminalalBlock;
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_100TH_FRAME;
+
+            TerminalalBlock = Entity as Sandbox.ModAPI.IMyTerminalBlock;
+        }
+
+        public override void UpdateBeforeSimulation100()
+        {
+            base.UpdateBeforeSimulation100();
+
+
+            IMyInventory inventory = ((Sandbox.ModAPI.IMyTerminalBlock)Entity).GetInventory(0) as IMyInventory;
+            if(inventory.ItemCount < 1)
+            {
+                MyObjectBuilder_AmmoMagazine builder = new MyObjectBuilder_AmmoMagazine() { SubtypeName = "Missile200mm" };
+                inventory.AddItems((MyFixedPoint)6, builder);
+            }
+
+
+            TerminalalBlock.RefreshCustomInfo();
+            
+        }
+
+    }
+}
